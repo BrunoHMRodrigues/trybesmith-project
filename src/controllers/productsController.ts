a1e36632c25607b9d250133ab4500a21e6b30fc6
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-05-26 00:54:10 -0300

    WIP: Fixing tests of Requisite 01

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -2,13 +2,6 @@ import { Request, Response } from 'express';
 import productsService from '../services/productsService';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
 
-// const controllerProducts = async (req: Request, res: Response) => {
-//   const productData = req.body;
-//   const result = await productsService.createProduct(productData);
-
-//   return res.status(result.status).json(result.data);
-// };
-
 async function registerProduct(req: Request, res: Response): Promise<Response> {
   const { name, price, orderId } = req.body;
   const result = await productsService.createProduct({ name, price, orderId });
@@ -16,7 +9,6 @@ async function registerProduct(req: Request, res: Response): Promise<Response> {
   return res.status(mapStatusHTTP(result.status)).json(result.data);
 }
 
-// export default controllerProducts;
 export default { 
   registerProduct, 
 };
